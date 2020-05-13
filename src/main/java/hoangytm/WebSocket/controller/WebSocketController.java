1acf6ba80d250d8c7d44fb33c34fc55c88e35729
hoangpn <hoangpn@vissoft.vn>
2020-05-13 23:31:01 +0700

    chưa fix bug trang login

---
+++ src/main/java/hoangytm/WebSocket/controller/WebSocketController.java
@@ -0,0 +1,31 @@
+package hoangytm.WebSocket.controller;
+import hoangytm.WebSocket.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+/**
+ * @author PhanHoang
+ * 5/13/2020
+ */
+@Controller
+public class WebSocketController {
+
+
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
